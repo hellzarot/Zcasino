1815daedc13faf918eae89eaed927e7fac39ef68
JulienLa <byker83@hotmail.fr>
2022-09-20 14:09:52 +0200

    Delete exo5temperaturewhile.py

--- exo5temperaturewhile.py
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/python3.10
-temp = float(input("quelle temperature fait-il ? : "))
-if temp < 18:
-    
-    print(temp, "cest vachemnt frais comme temperature")
-
-else :
-    print(temp, "ça va je peux sortir sans pull")
