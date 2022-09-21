bd751acc8047d3f77b20e6fc5b9c5b9ee49f6b4a
julien <julien@julien.fr>
2022-09-21 10:57:48 +0200

    import des modules , les tables de multi

---
+++ fichier.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3.10
+def table(a, b):
+    for i in range(0,b+1):
+        print(i, " X ", a, " = ", i * a)
+
+
---
+++ prog.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3.10
+from fichier import table
+
+numtable = int(input("Quel est la table de multiplication que vous voulez voir ? "))
+nombre = int(input("jusqu'à quel nombre de multiplication que vous voulez voir ? "))
+
+print(table(numtable,nombre))
+
